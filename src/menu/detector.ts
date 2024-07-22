dfe962de023f9ace75e5dd80e650d0913ec284f4
Ispusinov Artur Alekseevich <okolobaxha@yandex.ru>
2024-07-22 19:37:11 +0700

    1

--- src/menu/detector.ts
+++ src/menu/detector.ts
@@ -2,7 +2,18 @@ import { Menu } from "github.com/octarine-public/wrapper"
 import { BaseMenu } from "./base"
 
 export class DetectorMenu extends BaseMenu {
+	public readonly ShowAllyHeroes: Menu.Toggle
+
 	constructor(node: Menu.Node) {
 		super(node, "Xp ESP", false)
+		this.ShowAllyHeroes = this.Tree.AddToggle("Show ally creep", false)
+	}
+
+	public override ResetSettings(callback: () => void): void {
+		super.ResetSettings(callback)
+
+		this.ShowAllyHeroes.value = this.ShowAllyHeroes.defaultValue
+
+		callback()
 	}
 }
--- src/menu/tracker.ts
+++ src/menu/tracker.ts
@@ -2,33 +2,33 @@ import { Menu } from "github.com/octarine-public/wrapper"
 import { BaseMenu } from "./base"
 
 export class TrackerMenu extends BaseMenu {
-	public readonly showAllyCreeps: Menu.Toggle
-	public readonly showAllyHeroes: Menu.Toggle
-	public readonly size: Menu.Slider
-	public readonly timeToShow: Menu.Slider
-	public readonly opactity: Menu.Slider
-	public readonly disibleMin: Menu.Slider
+	public readonly ShowAllyCreeps: Menu.Toggle
+	public readonly ShowAllyHeroes: Menu.Toggle
+	public readonly Size: Menu.Slider
+	public readonly TimeToShow: Menu.Slider
+	public readonly Opactity: Menu.Slider
+	public readonly DisibleMin: Menu.Slider
 
 	constructor(node: Menu.Node) {
 		super(node, "Who got the creep", false)
-		this.showAllyCreeps = this.Tree.AddToggle("Show ally creeps", false)
-		this.showAllyHeroes = this.Tree.AddToggle("Show ally heroes", false)
-		this.size = this.Tree.AddSlider("Size", 30, 25, 50)
-		this.timeToShow = this.Tree.AddSlider("Time to show seconds", 2, 1, 5)
-		this.opactity = this.Tree.AddSlider("Opacity", 85, 40, 100)
-		this.disibleMin = this.Tree.AddSlider("Disable after N minutes", 15, 5, 60)
+		this.ShowAllyCreeps = this.Tree.AddToggle("Show ally creeps", false)
+		this.ShowAllyHeroes = this.Tree.AddToggle("Show ally heroes", false)
+		this.Size = this.Tree.AddSlider("Size", 30, 25, 50)
+		this.TimeToShow = this.Tree.AddSlider("Time to show seconds", 2, 1, 5)
+		this.Opactity = this.Tree.AddSlider("Opacity", 85, 40, 100)
+		this.DisibleMin = this.Tree.AddSlider("Disable after N minutes", 15, 5, 60)
 	}
 
 	public ResetSettings(callback: () => void): void {
 		super.ResetSettings(callback)
 
 		this.State.value = this.State.defaultValue
-		this.showAllyCreeps.value = this.showAllyCreeps.defaultValue
-		this.showAllyHeroes.value = this.showAllyHeroes.defaultValue
-		this.size.value = this.size.defaultValue
-		this.timeToShow.value = this.timeToShow.defaultValue
-		this.opactity.value = this.opactity.defaultValue
-		this.disibleMin.value = this.disibleMin.defaultValue
+		this.ShowAllyCreeps.value = this.ShowAllyCreeps.defaultValue
+		this.ShowAllyHeroes.value = this.ShowAllyHeroes.defaultValue
+		this.Size.value = this.Size.defaultValue
+		this.TimeToShow.value = this.TimeToShow.defaultValue
+		this.Opactity.value = this.Opactity.defaultValue
+		this.DisibleMin.value = this.DisibleMin.defaultValue
 		callback()
 	}
 }
