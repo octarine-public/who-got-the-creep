a4bc7a89638bd8db5a027111b32618cad490c15c
артур <okolobaxha@yandex.ru>
2024-07-22 14:52:28 +0700

    1

--- src/menu/who-got-the-creep.ts
+++ src/menu/who-got-the-creep.ts
@@ -2,33 +2,33 @@ import { Menu } from "github.com/octarine-public/wrapper"
 import { BaseMenu } from "./base"
 
 export class WhoGotTheCreepMenu extends BaseMenu {
-	public readonly showAllyCreeps: Menu.Toggle
-	public readonly showAllyHeroes: Menu.Toggle
-	public readonly size: Menu.Slider
-	public readonly timeToShow: Menu.Slider
-	public readonly opactity: Menu.Slider
-	public readonly disibleMin: Menu.Slider
+	// public readonly showAllyCreeps: Menu.Toggle
+	// public readonly showAllyHeroes: Menu.Toggle
+	// public readonly size: Menu.Slider
+	// public readonly timeToShow: Menu.Slider
+	// public readonly opactity: Menu.Slider
+	// public readonly disibleMin: Menu.Slider
 
 	constructor(node: Menu.Node) {
 		super(node, "Who got the creep", true)
-		this.showAllyCreeps = this.Tree.AddToggle("Show ally creeps", false)
-		this.showAllyHeroes = this.Tree.AddToggle("Show ally heroes", false)
-		this.size = this.Tree.AddSlider("Size", 30, 25, 50)
-		this.timeToShow = this.Tree.AddSlider("Time to show seconds", 2, 1, 5)
-		this.opactity = this.Tree.AddSlider("Opacity", 85, 40, 100)
-		this.disibleMin = this.Tree.AddSlider("Disable after N minutes", 15, 5, 60)
+		// this.showAllyCreeps = this.Tree.AddToggle("Show ally creeps", false)
+		// this.showAllyHeroes = this.Tree.AddToggle("Show ally heroes", false)
+		// this.size = this.Tree.AddSlider("Size", 30, 25, 50)
+		// this.timeToShow = this.Tree.AddSlider("Time to show seconds", 2, 1, 5)
+		// this.opactity = this.Tree.AddSlider("Opacity", 85, 40, 100)
+		// this.disibleMin = this.Tree.AddSlider("Disable after N minutes", 15, 5, 60)
 	}
 
-	public override ResetSettings(callback: () => void): void {
+	public ResetSettings(callback: () => void): void {
 		super.ResetSettings(callback)
 
 		this.State.value = this.State.defaultValue
-		this.showAllyCreeps.value = this.showAllyCreeps.defaultValue
-		this.showAllyHeroes.value = this.showAllyHeroes.defaultValue
-		this.size.value = this.size.defaultValue
-		this.timeToShow.value = this.timeToShow.defaultValue
-		this.opactity.value = this.opactity.defaultValue
-		this.disibleMin.value = this.disibleMin.defaultValue
+		// this.showAllyCreeps.value = this.showAllyCreeps.defaultValue
+		// this.showAllyHeroes.value = this.showAllyHeroes.defaultValue
+		// this.size.value = this.size.defaultValue
+		// this.timeToShow.value = this.timeToShow.defaultValue
+		// this.opactity.value = this.opactity.defaultValue
+		// this.disibleMin.value = this.disibleMin.defaultValue
 		callback()
 	}
 }
